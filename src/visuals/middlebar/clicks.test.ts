21a9fd5ca809b6932c0e5cf3b8391661b9942a8b
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-10-24 15:19:06 -0400

    ✅  Test clear clicks properly

--- src/visuals/middlebar/clicks.test.ts
+++ src/visuals/middlebar/clicks.test.ts
@@ -1,25 +1,22 @@
 import evaluate from "./evaluate";
 import clear from "./clear";
 import editor from "../monaco/setupMonaco";
-jest.mock("./evaluate", ()=>jest.fn());
-jest.mock("./clear", ()=>jest.fn());
+jest.mock("./evaluate", () => jest.fn());
+jest.mock("./clear", () => jest.fn());
 jest.mock("../monaco/setupMonaco");
 
-describe("Running code works properly", ()=>{
-    document.body.innerHTML = `
+document.body.innerHTML = `
     <button id="run">Run</button>
     <button id="clear">Clear</button>
     `;
-    require("./clicks");
-    test("Clicking run triggers evaluation", ()=>{
-        // @ts-ignore
-        editor.getValue.mockImplementation(()=>"console.log(1)");
-        document.getElementById("run").click();
-        expect(evaluate).toBeCalledWith("console.log(1)");
-    });
-    test("Clicking clear triggers clear", ()=>{
-        document.getElementById("clear").click();
-        expect(clear).toBeCalledTimes(1);
-    });
-    //TODO: Test the clear button
+require("./clicks");
+test("Clicking run triggers evaluation", () => {
+    // @ts-ignore
+    editor.getValue.mockImplementation(() => "console.log(1)");
+    document.getElementById("run").click();
+    expect(evaluate).toBeCalledWith("console.log(1)");
+});
+test("Clicking clear triggers clear", () => {
+    document.getElementById("clear").click();
+    expect(clear).toBeCalledTimes(1);
 });
