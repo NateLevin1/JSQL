60d6a54f0989d5f4b4e3d375d43b93c067eb9599
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-10-24 21:53:13 -0400

    ✅  Update tests for deleteTable

--- src/lib/runner/drop/deleteTable.test.ts
+++ src/lib/runner/drop/deleteTable.test.ts
@@ -40,3 +40,33 @@ test("rejects if db open fails", ()=>{
     database.db.open = jest.fn(()=>Promise.reject("r"));
     expect(()=>deleteTable("name", database as any)).rejects.toBe("There was a problem opening the database. r");
 });
+
+test("rejects if too long", ()=>{
+    jest.useFakeTimers();
+    // @ts-ignore
+    setTimeout.mockImplementationOnce((callback, time) => { callback(); });
+    database.db.open = jest.fn(()=>Promise.resolve());
+    expect(()=>deleteTable("name", database as any)).rejects.toBe("Opening the database took longer than expected.");
+    expect(setTimeout).toBeCalled();
+});
+
+test("works if db is not open", ()=>{
+    database.db.open = jest.fn(()=>Promise.resolve());
+    database.db.isOpen = jest.fn(()=>false);
+    deleteTable("name", database as any);
+    onCallback();
+    upgradeCallback();
+    expect(database.db.isOpen).toBeCalled();
+    expect(database.db.version).toBeCalled();
+});
+
+test("works if hasReadied is true", ()=>{
+    database.db.open = jest.fn(()=>Promise.resolve());
+    database.db.isOpen = jest.fn(()=>false);
+    deleteTable("name", database as any);
+    onCallback();
+    onCallback();
+    upgradeCallback();
+    expect(database.db.isOpen).toBeCalled();
+    expect(database.db.version).toBeCalled();
+});
