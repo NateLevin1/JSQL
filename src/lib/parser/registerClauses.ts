647ab5a89d7bdd42599a527132671a6f20370bba
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-11-02 17:58:48 -0500

    🐛  Allow for databases to be deleted

--- src/lib/parser/registerClauses.ts
+++ src/lib/parser/registerClauses.ts
@@ -8,4 +8,4 @@ clauses.register("CREATE", [["TABLE", "DATABASE"], "identifier", "expression"]);
 
 clauses.register("INSERT", ["INTO", "identifier", "expression"]);
 clauses.register("VALUES", ["rest"]); // TODO: probably shouldn't use rest here
-clauses.register("DROP", ["TABLE", "identifier"]);
+clauses.register("DROP", [["TABLE", "DATABASE"], "identifier"]);
